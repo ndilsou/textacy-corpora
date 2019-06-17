9a41ce7e228542cab04ac9732e120ac885bad013
ndilsou <nasoungadoy@gmail.com>
2019-06-17 14:04:13 +0100

    "[server] Bump version: 0.0.2 → 0.0.3"

--- src/textacy_corpora/about.py
+++ src/textacy_corpora/about.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-docstring
 
 __title__ = 'textacy_corpora'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __maintainer__ = 'Ndil-Adjim Sou Ngadoy'
 __maintainer_email__ = 'nasoungadoy@gmail.com'
 __description__ = 'An addon to textacy that allows to work with a labelled collection of Corpus'
